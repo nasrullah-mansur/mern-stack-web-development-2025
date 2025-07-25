bebd7a7dd1f71b43169db5f6bc9c1e0a32e2410b
ahmadsitweb <ahmadsitweb@gmail.com>
2025-07-25 20:44:33 +0600

    class-24

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,38 +1,140 @@
 
 
-// document.write('this is test');
-// console.log('this is log');
+// let output = 10;
 
-// window.alert("Hi kemon aso");
-
-// prompt('Tomar boyos koto?');
+// output -= 2;
 
 
-// var x = 10;
+// console.log(output);
 
-// let x = 10;
+// let age = 18;
 
-// const x = 10;
+// if(age == 18) {
+//     console.log("tumi boro hoye geco!!");
+// }
 
-
-// var test = 50;
-
-// var test = 100;
-
- 
-
-// let test = 10;
-
-// test = 20;
-
-// const test = 10;
-// test = 20;
-
-// console.log(test);
-
-// const webName = website.com 
+// else {
+//     console.log("tumi ekhono soto!!")
+// }
 
 
-let a = 3;
+let check = 200;
+
+// if(check === "10") {
+//     console.log('milce');
+// } 
+
+// if(check !== "10") {
+//     console.log('milce');
+// } 
+
+// if(check <= 10) {
+//     console.log('milce');
+// } 
+
+
+// if(check > 20) {
+//     console.log('20 theke boro');
+// }
+
+// else if(check > 10) {
+//     console.log('10 theke boro');
+// }
+
+// else {
+//     console.log('mileni');
+// }
+
+// check > 20 ? console.log('20 theke boro') : console.log('mileni');
+
+// console.log("hello" + " " + "world");
+
+// let one = "hello";
+
+// one += " world";
+
+// console.log(one);
+
+
+// let studentName = "khaled";
+
+// let studentAge = 20;
+
+// if(studentName == "khaled" || studentAge == 18) {
+//     console.log('this is actual student');
+// }
+
+
+// let test1 = 10;
+
+// if(!test1 == 10) {
+//     console.log('milce');
+// }
+
+// Truthy
+// 1. string 
+// 2. integer 
+// 3. array
+// 4. object 
+// 5. true
+
+// falsy 
+// 1. false
+// 2. null 
+// 3. undefined
+// 4. ""
+// 5. 0
+
+
+// if(!"abcd") {
+//     console.log('ami sottobadi');
+// }
+
+
+function test() {
+    let a = 10;
+    let output = "";
+
+    if(a = 10) {
+        output = "this is 10";
+    } 
+    else {
+        output = "this is empty value";
+    }
+
+    console.log(output);
+}
+
+
+// test();
+
+// Index number;
+
+let studentNames = ["khaled", "jayed", "rashed"];
+let myInfo = [
+    "Khaled",
+    25,
+    "Dhaka",
+    "01987878787",
+    "myemail@gmail.com",
+    "Bangladesh",
+    "Islam"
+];
+
+// key, value
+
+let myInfoObj = {
+    name: "khaled",
+    age: 25,
+    address: "Dhaka",
+    phoneNumber: "01728619733",
+    email: "nasrullah.cit.bd@gmail.com",
+    country: "Bangladesh",
+    religion: "Islam"
+}
+
+console.log(myInfo);
+console.log(myInfoObj);
+
 
 
