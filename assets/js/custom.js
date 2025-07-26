0780734caefd2c18968f654ea0657f8d5f8a42f7
ahmadsitweb <ahmadsitweb@gmail.com>
2025-07-26 21:38:58 +0600

    class-25

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -133,8 +133,140 @@ let myInfoObj = {
     religion: "Islam"
 }
 
-console.log(myInfo);
-console.log(myInfoObj);
+// console.log(myInfo);
+// console.log(myInfoObj);
+
+let myArr2 = [
+    "test data",
+    123,
+    undefined,
+    null,
+    [1,2,3,4],
+    function (){},
+    {
+        name: 'name'
+    }
+];
+
+let myObj2 = {
+    fName: "khaled",
+    lName: "hasan",
+    fullName: function() {
+        console.log(myObj2.fName + " " + myObj2.lName);
+    },
+    skill: ['php', 'javascript', 'html', 'css']
+}
+
+// console.log(myObj2);
+// console.log(myObj2.lName);
+
+// myObj2.fullName();
+
+
+
+function newFunction(fName, lName) {
+    console.log(fName + " " + lName);
+}
+
+// newFunction("khaled", "hassan");
+
+function anotherFn() {
+    
+    let a = 10;
+    let b = 20;
+
+    let output = a + b;
+
+    // console.log(output);
+
+    let authUser = true;
+
+    return true;
+}
+
+
+// console.log(anotherFn());
+// document.write(anotherFn());
+
+
+// let fName = "khaled";
+// let lName = "hassan";
+
+// let student = {
+//     // fName: fName,
+//     // lName: lName
+//     fName,
+//     lName,
+//     email: function(emailProperty) {
+//         console.log(emailProperty);
+//     }
+// }
+
+// // console.log(student);
+
+
+// student.email('email@email.com');
+
+
+
+// function test2(prop) {
+//     // let prop = 12345;
+//     console.log(prop);
+// }
+
+// test2(12345);
+// test2("00000000000000000000")
+// test2("hello dunia")
+
+
+
+// function test3(fname, lname) {
+//     console.log(fname + " " + lname);
+// }
+
+// test3("khaled", "hassan");
+// test3("jayed", "hassan");
+
+
+function test3(fname, lname, age) {
+    let studentName = fname + " " + lname;
+    let studentStatus = "";
+
+    if(age >= 18) {
+        studentStatus = "tumi boro hoye geso";
+    }
+
+    else {
+        studentStatus = "tumi ekhono soto";
+    }
+
+    let result = "hello " + studentName + " " + studentStatus;
+
+    return result;
+    
+
+}
+
+// "hello khaled hassan tumi boro hoye geso"
+test3("khaled", "hassan", 20);
+
+// console.log(test3("khaled", "hassan", 20));
+
+// let mrKhaled = test3("khaled", "hassan", 20);
+
+// console.log(mrKhaled);
+
+
+// let a = undefined;
+
+// console.log(a);
+
+
+function abc(a) {
+    console.log(a);
+}
+
+abc('hello');
 
 
 
