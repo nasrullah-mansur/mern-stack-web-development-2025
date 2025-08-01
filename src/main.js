6573ae27af625e00bf790e6c2c3a1d81523a0781
ahmadsitweb <ahmadsitweb@gmail.com>
2025-08-01 20:29:24 +0600

    class-30

--- src/main.js
+++ src/main.js
@@ -1,23 +1,9 @@
+let myP = document.querySelector('#my-p');
 
-let h1 = document.getElementById('test');
+myP.addEventListener("click", afterClickAction);
 
-// let myObj = {
-//     name: 'khaled',
-//     age: 25
-// }
+let count = 1;
 
-// myObj.name = 'another student';
-
-// myObj.address = "Dhaka"
-
-// console.log(myObj);
-
-// h1.innerText = "abol tabol"
-
-// console.dir(h1);
-// console.dir(h1.attributes);
-// console.dir(h1.classList);
-// console.dir(h1.innerHTML);
-// console.dir(h1.innerText);
-
-
+function afterClickAction() {
+    console.log("Count is: " + count++);
+}
